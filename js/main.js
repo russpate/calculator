9d6776fbf80ea9985cf01c6df5d737a2517cd0a7
Russell Pate <russ@russpate.com>
2016-05-16 22:22:14 -0400

    added some links into index.html

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  calcApp.init();
+});
+
+var calcApp = {
+  init: function(){
+    calcApp.events();
+  },
+  events: function(){
+    
+  }
+
+};
---
+++ js/templates.js
@@ -0,0 +1,7 @@
+var templates = {};
+
+templates.calc = [
+  '<div class="container">',
+    '<h1>Something</h1>',
+  '</div>'
+].join('');

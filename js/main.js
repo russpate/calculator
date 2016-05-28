d29bef219d3b82f9422062de99ac8c2a47fbf7d2
Russell Pate <russ@russpate.com>
2016-05-28 16:07:42 -0400

    initial functionality in place with basic style, next refactor js and add input functinality

--- js/main.js
+++ js/main.js
@@ -1,13 +1,78 @@
+// $(document).ready(function(){
+//   calcApp.init();
+// });
+//
+// var calcString = "";
+// var calcApp = {
+//   init: function(){
+//     calcApp.events();
+//     calcApp.toPage();
+//   },
+//   events: function(){
+//     $('#calc-main').on('click', 'span', calcApp.numLog);
+//   },
+//
+//   toPage: function(arr){
+//     $('#calc-main').html(templates.calc);
+//   }
+// };
+
 $(document).ready(function(){
-  calcApp.init();
+  //prevents the length of numbers from going outside the div, might remove this.
+	var testNumLength = function(number) {
+        if (number.length > 9) {
+            totaldiv.text(number.substr(number.length-9,9));
+            if (number.length > 15) {
+                number = "";
+                totaldiv.text("Err");
+            }
+        }
+    };
+	var number = "";
+  var newnumber = "";
+  var operator = "";
+  var totaldiv = $("#total");
+  totaldiv.text("0");
+
+  //click funciton for numbers
+  $("#numbers > a").not("#clear,#clearall").click(function(){
+		number += $(this).text();
+		totaldiv.text(number);
+		testNumLength(number);
+  });
+
+  // click function for operators
+  $("#operators > a").not("#equals").click(function(){
+		operator = $(this).text();
+		newnumber = number;
+		number = "";
+		totaldiv.text(operator);
+  });
+
+  // click function for clearing
+  $("#clear,#clearall").click(function(){
+		number = "";
+		totaldiv.text("0");
+		if ($(this).attr("id") === "clearall") {
+			newnumber = "";
+		}
+  });
+
+  // click function for equals
+  $("#equals").click(function(){
+   if (operator === "+"){
+     number = (parseInt(number, 10) + parseInt(newnumber,10)).toString(10);
+   } else if (operator === "-"){
+     number = (parseInt(newnumber, 10) - parseInt(number,10)).toString(10);
+   } else if (operator === "/"){
+     number = (parseInt(newnumber, 10) / parseInt(number,10)).toString(10);
+   } else if (operator === "*"){
+     number = (parseInt(newnumber, 10) * parseInt(number,10)).toString(10);
+   }
+   totaldiv.text(number);
+   testNumLength(number);
+   number = "";
+   newnumber = "";
+   });
+
 });
-
-var calcApp = {
-  init: function(){
-    calcApp.events();
-  },
-  events: function(){
-    
-  }
-
-};
--- js/templates.js
+++ js/templates.js
@@ -1,7 +1,32 @@
-var templates = {};
-
-templates.calc = [
-  '<div class="container">',
-    '<h1>Something</h1>',
-  '</div>'
-].join('');
+// var templates = {};
+//
+// templates.calc = [
+//   '<ul>',
+//     '<li id="clear">C</li>',
+//     '<li id="input"></li>',
+//   '</ul>',
+//   '<ul>',
+//     '<li class="calButton">7</li>',
+//     '<li class="calButton">8</li>',
+//     '<li class="calButton">9</li>',
+//     '<li class="calButton">/</li>',
+//   '</ul>',
+//   '<ul>',
+//     '<li class="calButton">4</li>',
+//     '<li class="calButton">5</li>',
+//     '<li class="calButton">6</li>',
+//     '<li class="calButton">X</li>',
+//   '</ul>',
+//   '<ul>',
+//     '<li class="calButton">1</li>',
+//     '<li class="calButton">2</li>',
+//     '<li class="calButton">3</li>',
+//     '<li class="calButton">-</li>',
+//   '</ul>',
+//   '<ul>',
+//     '<li class="calButton">0</li>',
+//     '<li class="calButton">.</li>',
+//     '<li id="evaluateCalc">=</li>',
+//     '<li class="calButton">+</li>',
+//   '</ul>'
+// ].join("");
